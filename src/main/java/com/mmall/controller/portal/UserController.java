7c85fd05aaeea8632c8e4b86e2aac422d5bf7b19
justiceofheaven <kolo924229628@163.com>
2019-06-25 23:42:04 +0800

    完善登录接口

--- src/main/java/com/mmall/controller/portal/UserController.java
+++ src/main/java/com/mmall/controller/portal/UserController.java
@@ -1,15 +1,24 @@
 package com.mmall.controller.portal;
 
+import com.mmall.common.Const;
+import com.mmall.common.ServerResponse;
+import com.mmall.pojo.User;
+import com.mmall.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/user/")
 public class UserController {
+
+    @Autowired
+    private IUserService iUserService;
 
     /**
      * ResponseBody() 将返回结果自动序列化为json，配置在dispatcher-servlet.xml
@@ -20,7 +29,11 @@ public class UserController {
      */
     @RequestMapping(value = "login.do",method = RequestMethod.POST)
     @ResponseBody()
-    public Object login(String username, String password, HttpSession session) {
-        return null;
+    public ServerResponse<User> login(String username, String password, HttpSession session) {
+        ServerResponse<User> response = iUserService.login(username, password);
+        if (response.isSuccess()) {
+            session.setAttribute(Const.CURRENT_USER,response.getData());
+        }
+        return response;
     }
 }
--- src/main/java/com/mmall/service/impl/UserServiceImpl.java
+++ src/main/java/com/mmall/service/impl/UserServiceImpl.java
@@ -4,8 +4,11 @@ import com.mmall.common.ServerResponse;
 import com.mmall.dao.UserMapper;
 import com.mmall.pojo.User;
 import com.mmall.service.IUserService;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service("iUserService")
 public class UserServiceImpl implements IUserService {
 
     @Autowired
@@ -13,6 +16,16 @@ public class UserServiceImpl implements IUserService {
 
     @Override
     public ServerResponse<User> login(String username, String password) {
-        return null;
+        int resultCount = userMapper.checkUserName(username);
+        if (resultCount == 0){
+            return ServerResponse.createByError("用户名不存在");
+        }
+
+        User user = userMapper.selectLogin(username, password);
+        if (user == null) {
+            return ServerResponse.createByError("密码错误");
+        }
+        user.setPassword(StringUtils.EMPTY);
+        return ServerResponse.createBySuccess("登陆成功",user);
     }
 }
