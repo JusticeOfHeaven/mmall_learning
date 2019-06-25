4c296c3bc8c8787782cdbf3b3dccb60dd615d472
jzhan <kolo924229628@163.com>
2019-06-25 21:01:21 +0800

    UserController、统一响应类

---
+++ src/main/java/com/mmall/controller/portal/UserController.java
@@ -0,0 +1,26 @@
+package com.mmall.controller.portal;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+@RequestMapping("/user/")
+public class UserController {
+
+    /**
+     * ResponseBody() 将返回结果自动序列化为json，配置在dispatcher-servlet.xml
+     * @param username
+     * @param password
+     * @param session
+     * @return
+     */
+    @RequestMapping(value = "login.do",method = RequestMethod.POST)
+    @ResponseBody()
+    public Object login(String username, String password, HttpSession session) {
+        return null;
+    }
+}
